7ee712c242bb2ce90c2c8ffa53d5380e41ac459f
Daniel Penaloza <juandanielpenalozabrito@gmail.com>
2024-03-25 22:16:24 -0500

    Fix imports issue

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,2 @@
-export { default as Experience } from './Experience';
-export { default as Footer } from './Footer';
-export { default as Navbar } from './Navbar';
-export { default as Services } from './Services';
-export { default as Skills } from './Skills';
 export { default as ThemeSwitcher } from './ThemeSwitcher';
 export * from './index';
