660df0c60a7c0d23b5fb7f552ef956d5886a2e57
Daniel Penaloza <juandanielpenalozabrito@gmail.com>
2024-04-15 19:10:59 -0500

    style: :lipstick: Change <hr /> border color

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function Custom404() {
 	return (
 		<div className="flex h-[calc(100vh-212px)] flex-col items-center justify-center">
 			<div className="relative flex flex-col items-center justify-center overflow-hidden">
-				<div className="absolute bottom-0 left-0 right-0 h-48 w-full bg-gradient-to-b from-white/80 via-white to-white" />
+				<div className="absolute bottom-0 left-0 right-0 h-48 w-full bg-gradient-to-b from-white/60 via-white to-white" />
 				<span className="font-montserrat text-[10rem] font-bold text-indigo-400">
 					404
 				</span>
@@ -15,7 +15,7 @@ export default function Custom404() {
 				<span className="text-xl font-semibold text-indigo-800">
 					This page couldn&apos;t be found
 				</span>
-				<hr />
+				<hr className="border-indigo-300" />
 				<Link
 					href={"/"}
 					className="flex w-min gap-2 rounded-lg bg-indigo-500 px-3 py-2 font-medium text-white hover:bg-indigo-400"
