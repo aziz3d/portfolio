f9896ec53cd6d1b619d2f98d89b3d6cc96aa4516
Daniel Penaloza <juandanielpenalozabrito@gmail.com>
2024-05-16 20:19:01 -0500

    Adding contact form functionality

---
+++ src/components/contact/ContactForm.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { contactFormSchema } from "@/schemas";
+import { useFormik } from "formik";
+import React from "react";
+
+export default function ContactForm() {
+	const onSubmit = async (values) => {
+		console.log(values);
+	};
+
+	const {
+		handleSubmit,
+		errors,
+		touched,
+		values,
+		handleBlur,
+		handleChange,
+		resetForm,
+		submitForm,
+		isValid,
+	} = useFormik({
+		initialValues: {
+			firstName: "",
+			lastName: "",
+			email: "",
+			message: "",
+		},
+		validationSchema: contactFormSchema,
+		onSubmit,
+	});
+
+	return (
+		<form onSubmit={handleSubmit} className="flex w-full flex-col gap-6">
+			<div className="grid grid-cols-2 gap-6">
+				<input
+					type="text"
+					name="firstName"
+					id="firstName"
+					placeholder="First name"
+					className="col-span-1 rounded-lg bg-white px-3 py-2 text-neutral-500 outline outline-1 outline-neutral-300 placeholder:text-neutral-500 focus:outline-2 focus:outline-indigo-300"
+					autoComplete="given-name"
+				/>
+				<input
+					type="text"
+					name="lastName"
+					id="lastName"
+					placeholder="Last name"
+					className="col-span-1 rounded-lg bg-white px-3 py-2 text-neutral-500 outline outline-1 outline-neutral-300 placeholder:text-neutral-500 focus:outline-2 focus:outline-indigo-300"
+					autoComplete="family-name"
+				/>
+				<input
+					type="email"
+					name="email"
+					id="email"
+					placeholder="Email"
+					className="col-span-2 rounded-lg bg-white px-3 py-2 text-neutral-500 outline outline-1 outline-neutral-300 placeholder:text-neutral-500 focus:outline-2 focus:outline-indigo-300"
+					autoComplete="email"
+				/>
+				<textarea
+					name="message"
+					id="message"
+					cols={30}
+					rows={10}
+					placeholder="Message"
+					className="col-span-2 max-h-[150px] rounded-lg bg-white px-3 py-2 text-neutral-500 outline outline-1 outline-neutral-300 placeholder:text-neutral-500 focus:outline-2 focus:outline-indigo-300"
+				></textarea>
+				<button
+					type="submit"
+					title="Submit"
+					className="col-span-2 rounded-lg bg-indigo-500 px-4 py-2.5 font-semibold leading-6 text-white hover:bg-indigo-400"
+				>
+					Submit
+				</button>
+			</div>
+		</form>
+	);
+}
--- src/components/contact/ContactSection.tsx
+++ src/components/contact/ContactSection.tsx
@@ -10,49 +10,6 @@ export default function ContactSection() {
 			className="flex w-full items-center justify-center py-48"
 		>
 			<div className="z-10 flex w-full max-w-[1280px] items-center gap-24 max-xl:px-4 max-lg:flex-col max-md:flex-col-reverse">
-				<form action="" className="flex w-full flex-col gap-6">
-					<div className="grid grid-cols-2 gap-6">
-						<input
-							type="text"
-							name="firstName"
-							id="firstName"
-							placeholder="First name"
-							className="col-span-1 rounded-lg bg-white px-3 py-2 text-neutral-500 outline outline-1 outline-neutral-300 placeholder:text-neutral-500 focus:outline-2 focus:outline-indigo-300"
-							autoComplete="given-name"
-						/>
-						<input
-							type="text"
-							name="lastName"
-							id="lastName"
-							placeholder="Last name"
-							className="col-span-1 rounded-lg bg-white px-3 py-2 text-neutral-500 outline outline-1 outline-neutral-300 placeholder:text-neutral-500 focus:outline-2 focus:outline-indigo-300"
-							autoComplete="family-name"
-						/>
-						<input
-							type="email"
-							name="email"
-							id="email"
-							placeholder="Email"
-							className="col-span-2 rounded-lg bg-white px-3 py-2 text-neutral-500 outline outline-1 outline-neutral-300 placeholder:text-neutral-500 focus:outline-2 focus:outline-indigo-300"
-							autoComplete="email"
-						/>
-						<textarea
-							name="message"
-							id="message"
-							cols={30}
-							rows={10}
-							placeholder="Message"
-							className="col-span-2 max-h-[150px] rounded-lg bg-white px-3 py-2 text-neutral-500 outline outline-1 outline-neutral-300 placeholder:text-neutral-500 focus:outline-2 focus:outline-indigo-300"
-						></textarea>
-						<button
-							type="submit"
-							title="Submit"
-							className="col-span-2 rounded-lg bg-indigo-500 px-4 py-2.5 font-semibold leading-6 text-white hover:bg-indigo-400"
-						>
-							Submit
-						</button>
-					</div>
-				</form>
 				<div className="flex flex-col justify-center gap-8 md:items-center">
 					<h1 className="font-montserrat text-5xl font-bold text-indigo-900">
 						Let&apos;s connect
